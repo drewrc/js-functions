ca468a5f82ecd8fff096fd82c5124e8a8a67ae72
Drew Coker <roselie@MacBook-Air.local>
2023-01-03 15:49:42 -0500

    completes maxOfThree function

--- js/exercises.js
+++ js/exercises.js
@@ -4,25 +4,43 @@
   // ---------------------
   // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
   // ---------------------
-
+function max(num1, num2) {
+  if(num1>num2) {
+    return num1;
+  } else {num2
+  }
+}
 
 
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
 
-
+function maxOfThree(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) {
+    return num1;
+  } else if 
+    (num2 > num1 && num2 > num3) 
+    {return num2;} else if (num3 > num1 && num3 > num2) {
+      return num3;
+    }}
+  
 
   // ---------------------
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
 
+isVowel(vow)
+ if (vow = 'A' || 'E' || 'I' || 'O' || 'U') {
+  return true;
+ } else {return false;}
 
 
   // ---------------------
   // Write a function rovarspraket() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
   // ---------------------
 
+rovarspraket()
 
 
   // ---------------------
